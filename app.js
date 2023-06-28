0dbb30ab0e7d20f6d93e047339ea4095c9bcdac9
Danny Mendez <dmendez4424@gmail.com>
2023-06-28 01:03:37 -0700

    Added color picker wheel and eraser mode

--- app.js
+++ app.js
@@ -1,55 +1,101 @@
-let canvas = document.querySelector('.canvas');
-let slider = document.querySelector('.slider');
-let output = document.querySelector('#sliderOutput');
+const canvas = document.querySelector('.canvas');
+const slider = document.querySelector('.slider');
+const output = document.querySelector('#sliderOutput');
+
+const clear = document.querySelector('#clear')
+
+clear.addEventListener('click', removePixels)
+clear.addEventListener('click', createCanvas)
+
+colorPicker = document.querySelector("#colorpicker");
+colorPicker.addEventListener("input", updateFirst);
+colorPicker.addEventListener("change", updateSecond);
+
+let toggleEraser = 1;
+const eraser = document.querySelector('#eraser')
+eraserMode = false;
+
+eraser.addEventListener('click', () => {
+    if (eraserMode % 2 === 0) {
+        eraserMode = true;
+        toggleEraser++;
+    } else {
+        eraserMode = false;
+        toggleEraser++;
+    }
+
+});
+
+
+function updateFirst(event) {
+    colorPicker.style.color = event.target.value;
+}
+function updateSecond(event) {
+    colorPicker.style.color = event.target.value;
+}
 
 
 function createCanvas () {
     let pixelAmount = slider.value;
     for (let i = 1; i <= pixelAmount**2; i++) {
         let pixel = document.createElement('div');
-        pixel.addEventListener('mousedown', startDrawing);
-        pixel.addEventListener('mouseover', drawPixel);
-        pixel.addEventListener('mouseup', stopDrawing);
+        pixel.setAttribute('draggable', false);
         pixel.classList.add('pixel');
+        pixel.classList.add('eraserMode');
         pixel.style.height = `${600 / pixelAmount}px`;
         pixel.style.width = `${600 / pixelAmount}px`;
         canvas.appendChild(pixel);
     }
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mouseover', drawPixel);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('mouseleave', stopDrawing)
 }
 
 let isDrawing = false;
 
+function disableDragging(e) {
+    e.preventDefault()
+}
+
 function startDrawing(e) {
     isDrawing = true;
-    e.target.style.backgroundColor = 'black';
+    if (isDrawing && e.buttons === 1 && e.target.classList.contains('eraserMode') && eraserMode) {
+        e.target.style.backgroundColor = 'transparent';
+        // removeProperty("backgroundColor")
+    }
+    else if (e.target.classList.contains('pixel')) {
+        e.target.style.backgroundColor = colorPicker.style.color;
+    }
 }
 
 function drawPixel(e) {
-    if (isDrawing) {
-        e.target.style.backgroundColor = 'black';
-    }
+    if (isDrawing && e.buttons === 1 && e.target.classList.contains('eraserMode') && eraserMode) {
+        e.target.style.backgroundColor = 'transparent';
+    } else if (isDrawing && e.buttons === 1 && e.target.classList.contains('pixel')) {
+        e.target.style.backgroundColor = colorPicker.style.color;
+    } 
 }
 
-function stopDrawing(e) {
-    if (isDrawing) {
-        e.target.style.backgroundColor = 'black';
-    }
+function stopDrawing() {
     isDrawing = false;
 }
+
 
 function removePixels () {
     let pixels = document.querySelectorAll('.pixel')
     while (canvas.hasChildNodes()) {
         canvas.removeChild(canvas.firstChild)
     }
-    console.log(pixels);
 
 }
+document.addEventListener('dragstart', disableDragging);
+
 slider.addEventListener('input', () => {
     removePixels();
     createCanvas();
     output.textContent = `Size: ${slider.value}x${slider.value}`;
 
-})
+});
 
 createCanvas();
